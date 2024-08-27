ad1218741cd16333e2a1fc10b0a1f0761b3c080b
KRath87 <k.rath@mbd-team.de>
2024-08-27 12:01:02 +0200

    läuft, aber weiterspielbar

--- src/main.ts
+++ src/main.ts
@@ -11,149 +11,200 @@ let player = "";
 let counter = 0;
 const arr: string[] = [];
 
-for (let counter = 1; counter < 10; counter++) {
-	(<HTMLDivElement>document.getElementById("1")).addEventListener(
-		"click",
-		() => move("1"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("2")).addEventListener(
-		"click",
-		() => move("2"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("3")).addEventListener(
-		"click",
-		() => move("3"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("4")).addEventListener(
-		"click",
-		() => move("4"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("5")).addEventListener(
-		"click",
-		() => move("5"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("6")).addEventListener(
-		"click",
-		() => move("6"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("7")).addEventListener(
-		"click",
-		() => move("7"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("8")).addEventListener(
-		"click",
-		() => move("8"),
-		{ once: true }
-	);
-	(<HTMLDivElement>document.getElementById("9")).addEventListener(
-		"click",
-		() => move("9"),
-		{ once: true }
-	);
-}
+//MARK: EventListener
+(<HTMLButtonElement>document.getElementById("newGame")).addEventListener(
+	"click",
+	() => window.location.reload()
+);
 
-function move(x: string) {
+(<HTMLDivElement>document.getElementById("1")).addEventListener(
+	"click",
+	() => move("1"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("2")).addEventListener(
+	"click",
+	() => move("2"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("3")).addEventListener(
+	"click",
+	() => move("3"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("4")).addEventListener(
+	"click",
+	() => move("4"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("5")).addEventListener(
+	"click",
+	() => move("5"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("6")).addEventListener(
+	"click",
+	() => move("6"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("7")).addEventListener(
+	"click",
+	() => move("7"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("8")).addEventListener(
+	"click",
+	() => move("8"),
+	{ once: true }
+);
+(<HTMLDivElement>document.getElementById("9")).addEventListener(
+	"click",
+	() => move("9"),
+	{ once: true }
+);
+
+//MARK:move
+function move(DivId: string) {
 	if (counter % 2 === 0) {
 		player = "X";
-		(<HTMLDivElement>document.getElementById(x)).innerHTML = player;
+		(<HTMLDivElement>document.getElementById(DivId)).innerHTML = player;
 		counter++;
-		let indexArray = parseInt(x);
+		let indexArray = parseInt(DivId);
 		arr[indexArray] = player;
+
+		// (<HTMLDivElement>document.getElementById(DivId)).removeEventListener(
+		// 	"click",
+		// 	() => move(DivId)
+		// );
+		// removeEventListener();
 		winCheck();
 	} else {
 		player = "O";
-		(<HTMLDivElement>document.getElementById(x)).innerHTML = player;
+		(<HTMLDivElement>document.getElementById(DivId)).innerHTML = player;
 		counter++;
-		let indexArray = parseInt(x);
+		let indexArray = parseInt(DivId);
 		arr[indexArray] = player;
+
+		// (<HTMLDivElement>document.getElementById(DivId)).removeEventListener(
+		// 	"click",
+		// 	() => move(DivId)
+		// );
+		// removeEventListener();
 		winCheck();
 	}
 }
-
+//MARK:winCheck
 function winCheck() {
 	// horizontal
-	if (
-		(arr[1] === "X" && arr[2] === "X" && arr[3] === "X") ||
-		(arr[1] === "O" && arr[2] === "O" && arr[3] === "O")
-	) {
+	if (arr[1] === player && arr[2] === player && arr[3] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
-			// counter = 10;
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
-			// counter = 10;
+			setTimeout(playerTwoWin, 50);
+			// counter = 11;
 		}
-	} else if (
-		(arr[4] === "X" && arr[5] === "X" && arr[6] === "X") ||
-		(arr[4] === "O" && arr[5] === "O" && arr[6] === "O")
-	) {
+		newGame();
+	} else if (arr[4] === player && arr[5] === player && arr[6] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
-	} else if (
-		(arr[7] === "X" && arr[8] === "X" && arr[9] === "X") ||
-		(arr[7] === "O" && arr[8] === "O" && arr[9] === "O")
-	) {
+		newGame();
+	} else if (arr[7] === player && arr[8] === player && arr[9] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
+		newGame();
 		// vertikal
-	} else if (
-		(arr[1] === "X" && arr[4] === "X" && arr[7] === "X") ||
-		(arr[1] === "O" && arr[4] === "O" && arr[7] === "O")
-	) {
+	} else if (arr[1] === player && arr[4] === player && arr[7] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
-	} else if (
-		(arr[2] === "X" && arr[5] === "X" && arr[8] === "X") ||
-		(arr[2] === "O" && arr[5] === "O" && arr[8] === "O")
-	) {
+		newGame();
+	} else if (arr[2] === player && arr[5] === player && arr[8] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
-	} else if (
-		(arr[3] === "X" && arr[6] === "X" && arr[9] === "X") ||
-		(arr[3] === "O" && arr[6] === "O" && arr[9] === "O")
-	) {
+		newGame();
+	} else if (arr[3] === player && arr[6] === player && arr[9] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
+		newGame();
 		// diagonal
-	} else if (
-		(arr[1] === "X" && arr[5] === "X" && arr[9] === "X") ||
-		(arr[1] === "O" && arr[5] === "O" && arr[9] === "O")
-	) {
+	} else if (arr[1] === player && arr[5] === player && arr[9] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
-	} else if (
-		(arr[3] === "X" && arr[5] === "X" && arr[7] === "X") ||
-		(arr[3] === "O" && arr[5] === "O" && arr[7] === "O")
-	) {
+		newGame();
+	} else if (arr[3] === player && arr[5] === player && arr[7] === player) {
 		if (player === "X") {
-			alert("Glückwunsch Spieler 1");
+			setTimeout(playerOneWin, 50);
 		} else {
-			alert("Glückwunsch Spieler 2");
+			setTimeout(playerTwoWin, 50);
 		}
+		newGame();
 	}
 }
+//MARK: timeOut
+function playerOneWin() {
+	alert("Glückwunsch Spieler 1.");
+}
+function playerTwoWin() {
+	alert("Glückwunsch Spieler 2");
+}
+//MARK: "newGame";
+function newGame() {
+	(<HTMLButtonElement>document.getElementById("newGame")).classList.add("show");
+}
+
+//MARK: "removeEventListener";
+// function removeEventListener() {
+// 	(<HTMLDivElement>document.getElementById("1")).removeEventListener(
+// 		"click",
+// 		() => move("1")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("2")).removeEventListener(
+// 		"click",
+// 		() => move("2")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("3")).removeEventListener(
+// 		"click",
+// 		() => move("3")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("4")).removeEventListener(
+// 		"click",
+// 		() => move("4")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("5")).removeEventListener(
+// 		"click",
+// 		() => move("5")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("6")).removeEventListener(
+// 		"click",
+// 		() => move("6")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("7")).removeEventListener(
+// 		"click",
+// 		() => move("7")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("8")).removeEventListener(
+// 		"click",
+// 		() => move("8")
+// 	);
+// 	(<HTMLDivElement>document.getElementById("9")).removeEventListener(
+// 		"click",
+// 		() => move("9")
+// 	);
+// }
